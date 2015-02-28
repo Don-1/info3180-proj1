05b0ec43ef6f932908962b98a4d527bb49c333a3
don-1 <don-saynomore@hotmail.com>
2015-02-28 04:35:19 +0000

    files updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
-import os
-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+UPLOAD_FOLDER = 'app/static/img'
+
 app = Flask(__name__)
-from app import views
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.form_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
 db = SQLAlchemy(app)
 
-#
-app.config['SECRET_KEY'] = 'it wo'
+from app import views, models
+
+app.config['SECRET_KEY'] = 'it works!'
